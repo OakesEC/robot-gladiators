a95b00224e0633b921cdfc1fe48b752a00174a6d
Liz Oakes <liz77oakes@gmail.com>
2021-03-01 17:46:23 -0500

    ask BCS 2

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,22 @@
-  
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-var playerAttack = 30;
-var playerMoney = 10; 
+var playerAttack = 10;
+var playerMoney = 10;
 
-// You can also multiple values at once like this
+// You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roberto";
+var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
-// Alert players that they are starting the round
-window.alert("Welcome to Robot Gladiators!");
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
 
-var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle?  Enter 'FIGHT' or 'SKIP' to choose.");
-console.log(promptFight);
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-// if player choses to fight, then fight
+ // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
@@ -47,11 +45,18 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
     // if player choses to skip
   } else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(playerName + " has chosen to skip the fight!");
-  } else {
-    window.alert("You need to choose a valid option. Try again!");
-  }
+    // confirm player wants to quit
+    var confirmSkip = window.confirm("Are you sure you want to quit?");
 
-  
-      fight();
+    // if yes (true), leave fight
+    if (confirmSkip) {
+        window.alert(playerName + " has decided to skip this fight.  Goodbye!");
+        //subtract money from playerMoney for skipping
+        playerMoney = playerMoney - 2;
     }
+    // if no (false), ask question again by running fight() again
+    else {
+        fight();
+    }
+  }
+}
